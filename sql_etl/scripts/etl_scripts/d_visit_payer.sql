f24ba3c6af3175917cc6219622ba60f2ec7c6548
Burrows, Evanette K <BURROWSE@EMAIL.CHOP.EDU>
2022-02-02 16:47:53 -0500

    Update visit payer script

--- sql_etl/scripts/etl_scripts/d_visit_payer.sql
+++ sql_etl/scripts/etl_scripts/d_visit_payer.sql
@@ -11,7 +11,7 @@ with payer_info as (
 select
 	split_part(source_concept_id,'-',1) as plan_class,
 	split_part(source_concept_id,'-',2)as plan_type,
-	raw_payer_name_primary as plan_name,
+	coalesce(raw_payer_name_primary,source_concept_id) as plan_name,
 	vo.visit_occurrence_id,
 	31968::int as visit_payer_type_concept_id -- primary
 from SITE_pcornet.encounter enc
@@ -26,21 +26,21 @@ union
 select
 	split_part(source_concept_id,'-',1) as plan_class,
 	split_part(source_concept_id,'-',2)as plan_type,
-	raw_payer_name_secondary as plan_name,
+	coalesce(raw_payer_name_secondary,source_concept_id) as plan_name,
 	vo.visit_occurrence_id,
-	31969::int as visit_payer_type_concept_id -- primary
+	31969::int as visit_payer_type_concept_id -- secondary
 from SITE_pcornet.encounter enc
 inner join SITE_pedsnet.visit_occurrence vo 
 	on enc.encounterid=vo.visit_source_value
 left join pcornet_maps.pedsnet_pcornet_valueset_map payer_map
-	on enc.payer_type_primary=payer_map.target_concept
+	on enc.payer_type_secondary=payer_map.target_concept
 	and payer_map.source_concept_class='Payer'
-where payer_type_primary is not null 
-	and payer_type_primary <>'NI'
+where payer_type_secondary is not null 
+	and payer_type_secondary <>'NI'
 )
 select
-	coalesce(payer_info.plan_class,'Other/Unknown'),
-	payer_info.plan_type,
+	coalesce(payer_info.plan_class,'Other/Unknown') as plan_class,
+	coalesce(payer_info.plan_type,'Other/Unknown') as plan_type,
 	payer_info.plan_name,
 	row_number() over (order by visit_occurrence_id)::bigint as visit_payer_id,
 	payer_info.visit_occurrence_id,
