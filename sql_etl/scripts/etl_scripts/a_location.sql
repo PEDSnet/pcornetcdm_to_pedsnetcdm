0ba57752d0b29113a5b6dc2d1249135c43dcc833
Sam Boss <bossa@chop.edu>
2022-02-02 15:19:20 -0500

    location.sql and location_history.sql fixes

--- sql_etl/scripts/etl_scripts/a_location.sql
+++ sql_etl/scripts/etl_scripts/a_location.sql
@@ -1,3 +1,5 @@
+create sequence if not exists SITE_pedsnet.loc_seq;
+
 begin;
 
 INSERT INTO SITE_pedsnet.location(
@@ -6,13 +8,26 @@ INSERT INTO SITE_pedsnet.location(
     location_source_value,
     site)
 select 
-	distinct
-		dense_rank() over (order by facility_location)::bigint  AS location_id,
+		nextval('SITE_pedsnet.loc_seq')::bigint AS location_id,
 		facility_location as zip,
 		facility_location as location_source_value,
 		'SITE' as site
  FROM SITE_pcornet.encounter enc
- WHERE enc.facility_type IS NOT NULL;
+ WHERE enc.facility_type IS NOT NULL
+ GROUP BY facility_location;
+
+INSERT INTO SITE_pedsnet.location(
+    location_id,
+    zip,
+    location_source_value,
+    site)
+select 
+	nextval('SITE_pedsnet.loc_seq')::bigint AS location_id,
+	address_zip5 as zip,
+	address_zip5 as location_source_value,
+	'SITE' as site
+ FROM SITE_pcornet.lds_address_history lds
+ GROUP BY address_zip5;
 
  -- default location
 
--- sql_etl/scripts/etl_scripts/f_location_history.sql
+++ sql_etl/scripts/etl_scripts/f_location_history.sql
@@ -12,22 +12,24 @@ insert into SITE_pedsnet.location_history(
 	location_preferred_concept_id, 
 	relationship_type_concept_id, 
 	start_date, 
-	start_datetime
+	start_datetime,
+	site
 )
 select
 	nextval('SITE_pedsnet.loc_hist_seq')::bigint as location_history_id,
-	'Person',
-	address_period_end::date,
-	address_period_end::timestamp,
+	'Person' as domain_id,
+	address_period_end::date as end_date,
+	address_period_end::timestamp as end_datetime,
 	person.person_id as entity_id,
-	loc.location_id,
-	44814653,
-	0,
-	address_period_start::date,
-	address_period_start::timestamp
+	loc.location_id as location_id,
+	44814653 as location_preferred_concept_id,
+	0 as relationship_type_concept_id,
+	address_period_start::date as start_date,
+	address_period_start::timestamp as start_datetime,
+	'SITE' as site
 from SITE_pcornet.lds_address_history lds
 inner join SITE_pedsnet.person person on lds.patid=person.person_source_value
-left join SITE_pedsnet.location loc on address_zip5=loc.zip
+inner join SITE_pedsnet.location loc on address_zip5=loc.zip
 ;
 
 commit;
