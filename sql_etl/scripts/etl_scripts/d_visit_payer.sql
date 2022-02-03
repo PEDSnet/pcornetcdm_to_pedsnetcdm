421e88ff84b9d26994199d4e63419203ef284f6d
Sam Boss <bossa@chop.edu>
2022-02-03 15:43:21 -0500

    visit_payer.sql

--- sql_etl/scripts/etl_scripts/d_visit_payer.sql
+++ sql_etl/scripts/etl_scripts/d_visit_payer.sql
@@ -1,3 +1,5 @@
+CREATE SEQUENCE if not exists SITE_pedsnet.visit_payer_seq;
+
 begin;
 
 insert into SITE_pedsnet.visit_payer(
@@ -41,10 +43,10 @@ where payer_type_secondary is not null
 )
 select
 	coalesce(payer_info.plan_class,'Other/Unknown') as plan_class,
+	payer_info.plan_name as plan_name,
 	coalesce(payer_info.plan_type,'Other/Unknown') as plan_type,
-	payer_info.plan_name,
-	row_number() over (order by visit_occurrence_id)::bigint as visit_payer_id,
 	payer_info.visit_occurrence_id,
+	nextval('SITE_pedsnet.visit_payer_seq')::bigint as visit_payer_id,
 	payer_info.visit_payer_type_concept_id,
 	'SITE' as site
 from payer_info;
