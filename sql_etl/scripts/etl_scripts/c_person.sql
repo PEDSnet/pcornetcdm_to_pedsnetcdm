1838ad0f1cc53fdfd00e437be1a872bbfc1e173d
Sam Boss <bossa@chop.edu>
2022-02-16 12:25:02 -0500

    provider_id for person table

--- sql_etl/scripts/etl_scripts/c_person.sql
+++ sql_etl/scripts/etl_scripts/c_person.sql
@@ -24,6 +24,52 @@ insert into SITE_pedsnet.person (
     race_source_concept_id, 
     race_source_value, 
     site)
+
+--derive a primary provider for each patient
+--find provider with most visits from patient and set as primary
+-- if > 1 providers have same max number of visits, takes most recent visit 
+with provider_history as (
+select
+	demo.patid, 
+	enc.providerid, 
+	max((enc.admit_date + enc.admit_time::time)::timestamp) as most_recent_date, 
+	count(enc.providerid) as num_visits
+from SITE_pcornet.demographic demo
+left join SITE_pcornet.encounter enc
+on demo.patid = enc.patid
+group by 
+	demo.patid, 
+	enc.providerid
+),
+get_provider_max as (
+select 
+	provider_history.patid, 
+	provider_history.providerid, 
+	provider_history.num_visits,
+	provider_history.most_recent_date
+from provider_history
+inner join (
+	select patid, max(num_visits) as max_visit
+		from provider_history
+		group by patid
+	) as most_visits 
+	on provider_history.patid = most_visits.patid
+	and provider_history.num_visits = most_visits.max_visit
+order by patid, most_recent_date desc
+),
+primary_provider as (
+select 
+	pcor_prov.patid,
+	peds_prov.provider_id
+from (	
+	select patid, max(providerid) as providerid
+	from get_provider_max
+	group by patid 
+) as pcor_prov
+inner join SITE_pedsnet.provider peds_prov
+	on pcor_prov.providerid = peds_prov.provider_source_value
+)
+
 SELECT 
     distinct on (demo.patid) demo.patid::bigint AS person_id, 
     demo.birth_date::date as birth_date,
@@ -44,12 +90,13 @@ SELECT
     9999999::bigint AS location_id,
     demo.patid AS person_source_value, 
     null as pn_gestational_age, 
-    NULL AS PROVIDER_ID, 
+    primary_provider.provider_id AS provider_id, 
     coalesce(ethnicity_map.sourcE_concept_id::int,44814650) race_concept_id,
     0 AS race_source_concept_id, 				
     demo.raw_race AS race_source_value,
     'SITE' as site
 FROM SITE_pcornet.DEMOGRAPHIC demo
+left join primary_provider on primary_provider.patid = demo.patid
 left join pcornet_maps.pedsnet_pcornet_valueset_map lang on source_concept_class = 'Language' 
            and source_concept_id is not null 
 		   and lang.target_concept = demo.pat_pref_language_spoken
