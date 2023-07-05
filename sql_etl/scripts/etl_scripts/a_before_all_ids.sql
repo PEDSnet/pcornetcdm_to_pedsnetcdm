f7ff6748a87a5e5f8e2e7fa7b886675c734a28fb
Sam Boss <bossa@chop.edu>
2023-07-05 12:00:13 -0400

    add missing varchar conversions

--- sql_etl/scripts/etl_scripts/a_before_all_ids.sql
+++ sql_etl/scripts/etl_scripts/a_before_all_ids.sql
@@ -1,6 +1,21 @@
 ALTER TABLE SITE_pedsnet.adt_occurrence
 	ALTER COLUMN adt_occurrence_id TYPE CHARACTER VARYING(256);
 
+ALTER TABLE SITE_pedsnet.adt_occurrence
+	ALTER COLUMN next_adt_occurrence_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.adt_occurrence
+	ALTER COLUMN prior_adt_occurrence_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.adt_occurrence
+	ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);   
+
+ALTER TABLE SITE_pedsnet.adt_occurrence
+	ALTER COLUMN person_id TYPE CHARACTER VARYING(256);   
+
+ALTER TABLE SITE_pedsnet.adt_occurrence
+	ALTER COLUMN care_site_id TYPE CHARACTER VARYING(256);   
+
 ALTER TABLE SITE_pedsnet.care_site     
     ALTER COLUMN care_site_id TYPE CHARACTER VARYING(256);
 
@@ -13,6 +28,9 @@ ALTER TABLE SITE_pedsnet.condition_occurrence
 ALTER TABLE SITE_pedsnet.condition_occurrence
     ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);
 
+ALTER TABLE SITE_pedsnet.condition_occurrence
+    ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
+
 ALTER TABLE SITE_pedsnet.death
     ALTER COLUMN death_cause_id TYPE CHARACTER VARYING(256);
 
@@ -22,8 +40,20 @@ ALTER TABLE SITE_pedsnet.drug_exposure
 ALTER TABLE SITE_pedsnet.drug_exposure
     ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);
 
+ALTER TABLE SITE_pedsnet.drug_exposure
+    ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
+
 ALTER TABLE SITE_pedsnet.device_exposure
     ALTER COLUMN device_exposure_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.device_exposure
+    ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.device_exposure
+    ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.device_exposure
+    ALTER COLUMN person_id TYPE CHARACTER VARYING(256);
 
 ALTER TABLE SITE_pedsnet.immunization
     ALTER COLUMN immunization_id TYPE CHARACTER VARYING(256);
@@ -33,6 +63,9 @@ ALTER TABLE SITE_pedsnet.immunization
 
 ALTER TABLE SITE_pedsnet.immunization
     ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.immunization
+    ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
 
 ALTER TABLE SITE_pedsnet.location    
     ALTER COLUMN location_id TYPE CHARACTER VARYING(256);
@@ -55,11 +88,17 @@ ALTER TABLE SITE_pedsnet.measurement
 ALTER TABLE SITE_pedsnet.measurement_organism
     ALTER COLUMN meas_organism_id TYPE CHARACTER VARYING(256);
 
+ALTER TABLE SITE_pedsnet.measurement     
+    ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
+
 ALTER TABLE SITE_pedsnet.observation       
     ALTER COLUMN observation_id TYPE CHARACTER VARYING(256);
 
 ALTER TABLE SITE_pedsnet.observation
     ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.observation
+    ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
 
 ALTER TABLE SITE_pedsnet.observation_period
     ALTER COLUMN observation_period_id TYPE CHARACTER VARYING(256);
@@ -103,6 +142,9 @@ ALTER TABLE SITE_pedsnet.procedure_occurrence
 ALTER TABLE SITE_pedsnet.procedure_occurrence
     ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);
 
+ALTER TABLE SITE_pedsnet.procedure_occurrence
+    ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
+
 ALTER TABLE SITE_pedsnet.provider            
     ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
 
@@ -110,7 +152,16 @@ ALTER TABLE SITE_pedsnet.visit_occurrence
     ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);
 
 ALTER TABLE SITE_pedsnet.visit_occurrence
+    ALTER COLUMN preceding_visit_occurrence_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.visit_occurrence
+    ALTER COLUMN admitted_from_concept_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.visit_occurrence
     ALTER COLUMN care_site_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.visit_occurrence
+    ALTER COLUMN provider_id TYPE CHARACTER VARYING(256);
 
 ALTER TABLE SITE_pedsnet.visit_payer     
     ALTER COLUMN visit_payer_id TYPE CHARACTER VARYING(256);
@@ -118,11 +169,23 @@ ALTER TABLE SITE_pedsnet.visit_payer
 ALTER TABLE SITE_pedsnet.visit_payer
     ALTER COLUMN visit_occurrence_id TYPE CHARACTER VARYING(256);
 
-ALTER TABLE SITE_pedsnet.condition_era
-    ALTER COLUMN condition_era_id TYPE CHARACTER VARYING(256);
+-- ALTER TABLE SITE_pedsnet.condition_era
+--     ALTER COLUMN condition_era_id TYPE CHARACTER VARYING(256);
 
-ALTER TABLE SITE_pedsnet.dose_era
-    ALTER COLUMN dose_era_id TYPE CHARACTER VARYING(256);
+-- ALTER TABLE SITE_pedsnet.dose_era
+--     ALTER COLUMN dose_era_id TYPE CHARACTER VARYING(256);
 
-ALTER TABLE SITE_pedsnet.drug_era
-    ALTER COLUMN drug_era_id TYPE CHARACTER VARYING(256);
+-- ALTER TABLE SITE_pedsnet.drug_era
+--     ALTER COLUMN drug_era_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.location_fips
+    ALTER COLUMN geocode_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.location_fips
+    ALTER COLUMN location_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.specialty
+    ALTER COLUMN entity_id TYPE CHARACTER VARYING(256);
+
+ALTER TABLE SITE_pedsnet.specialty
+    ALTER COLUMN specialty_id TYPE CHARACTER VARYING(256);
